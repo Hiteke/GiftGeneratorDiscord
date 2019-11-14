b843ee645741eda9f608fe8ae76aee70f0f8abc2
Hiteke <38786753+Hiteke@users.noreply.github.com>
2019-11-14 17:11:14 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const request = require('request');
+const generator = require('generate-password');
+var fs = require('fs');
+
+var i = 0;
+var i1 = 0;
+var c = 0;
+var arr = [];
+
+write();
+a();
+
+function a() {
+	setTimeout(write, 5000);
+var id = generator.generate({
+    length: 16,
+    numbers: true
+})
+
+request({url: `https://discordapp.com/api/v6/entitlements/gift-codes/${id}`, json: true}, function(err, res, json) {
+	i++;
+try{ if (json.message === 'You are being rate limited.') setTimeout(a, 60000); else a(); }
+catch(err) {
+	 c++;
+	 fs.appendFileSync("gifts.txt", "\nhttps://discord.gift/"+id);
+	 arr.push(json);
+	 fs.appendFileSync("log.txt", "\n"+json);
+	 a();
+}
+})
+}
+
+function write() {
+		console.clear();
+		console.log(c + ' of ' + i);
+for (i1 = 0; i1 < arr.length; i1) { 
+		console.log(arr[a]);
+		}
+	setTimeout(write, 5000);
+}
